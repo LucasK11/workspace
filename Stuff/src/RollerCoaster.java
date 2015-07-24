d9ded345735e94b19e926c7f9a3ea5c4efce7069
LucasK11 <Guest@wts9.attlocal.net>
2015-07-23 20:08:50 -0700

    I made a roller coster

---
+++ Stuff/src/RollerCoaster.java
@@ -0,0 +1,92 @@
+import javax.swing.JOptionPane;
+
+
+public class RollerCoaster {
+public static void main(String[] args) {
+String i =	JOptionPane.showInputDialog(null, "How tall are you?");
+
+	
+	int hi = Integer.parseInt(i);	
+	if(hi>=4){
+	
+	JOptionPane.showMessageDialog(null, "No it's time to Go on the roller coaster!!!!!!");
+	
+	
+	
+	}
+		else if(hi<4){
+	
+
+			JOptionPane.showMessageDialog(null, "Sorry, your too small, come back when when you have grown more.");
+	
+	
+	
+		}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
